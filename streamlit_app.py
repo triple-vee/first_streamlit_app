6b96bd909f5af1fa192eff9c48e3616fd4c598ce
Triple Vee LLC <7876083+triple-vee@users.noreply.github.com>
2022-07-19 05:45:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parent's New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinache & Rocket Smoothie")
+streamit.text("Hard-Boiled Free-Range Egg")
