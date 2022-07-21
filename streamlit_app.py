d72ead1fe4e6b4b68b755c297908d9d4e8f1fccc
Triple Vee LLC <7876083+triple-vee@users.noreply.github.com>
2022-07-22 00:29:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.text("Thanks for adding " + add_my_fruit)
 
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('values from streamlit');
