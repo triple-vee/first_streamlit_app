65d01c86129eb7902297899f4eea9d636adb9350
Triple Vee LLC <7876083+triple-vee@users.noreply.github.com>
2022-07-21 09:38:24 +0200

    pretty print the json

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# shred the json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display the json
+streamlit.dataframe(fruityvice_normalized)
