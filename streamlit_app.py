c3239da0a51ad0c30c462817cff76db9e33677c7
Triple Vee LLC <7876083+triple-vee@users.noreply.github.com>
2022-07-22 13:00:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 streamlit.header("The fruit load list contains:")
 
-def get_fruit_load_list()
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
